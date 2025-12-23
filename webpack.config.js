a55bade929272bf45a020693da1a4952c34cf1e5
PalomaFuerte <pfuerte301@gmail.com>
2025-12-23 13:19:52 +0100

    css

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,40 @@
 const path = require('path');
-const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './assets/js/script.js',
-
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: './',
-    clean: true,
+    path: path.resolve(__dirname, 'docs'), // carpeta final para GitHub Pages
+    publicPath: '', // paths relativos correctos
+    clean: true, // limpia docs antes de cada build
   },
-
-  mode: 'development',
-
+  mode: 'production',
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
+        use: { loader: 'babel-loader', options: { presets: ['@babel/preset-env'] } },
       },
-
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader'], // CSS incluido en bundle
       },
-
       {
-        test: /\.(png|jpe?g|gif|svg)$/i,
+        test: /\.(png|jpe?g|gif|svg|ico)$/i,
         type: 'asset/resource',
-        generator: {
-          filename: 'img/[name][ext]',
-        },
+        generator: { filename: 'img/[name][ext]' }, // copia imágenes a docs/img/
       },
     ],
   },
-
   plugins: [
-    new Dotenv(),
-
     new HtmlWebpackPlugin({
       template: './index.html',
-      inject: 'body', // inserisce <script> prima di </body>
+      favicon: './assets/img/favicon.ico', // favicon copiado a docs/
     }),
+    new Dotenv(), // si usas variables de entorno
   ],
-
-  devtool: 'source-map',
+  devtool: 'source-map', // para debug
 };
